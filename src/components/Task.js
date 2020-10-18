d993362fa22444f047a5783a393f281e5d2e67ca
Hasham Vakani <hashamvakani@users.noreply.github.com>
2020-10-19 00:03:42 +0500

    change task background to red

--- src/components/Task.js
+++ src/components/Task.js
@@ -16,14 +16,14 @@ export default function Task({ task: { id, title, state }, onArchiveTask, onPinT
         <span className="checkbox-custom" onClick={() => onArchiveTask(id)} />
       </label>
       <div className="title">
-  <input
-    type="text"
-    value={title}
-    readOnly={true}
-    placeholder="Input title"
-    style={{ background: 'red' }}
-  />
-</div>
+        <input
+          type="text"
+          value={title}
+          readOnly={true}
+          placeholder="Input title"
+          style={{ background: 'red' }}
+        />
+      </div>
 
       <div className="actions" onClick={event => event.stopPropagation()}>
         {state !== 'TASK_ARCHIVED' && (
@@ -38,17 +38,17 @@ export default function Task({ task: { id, title, state }, onArchiveTask, onPinT
 }
 
 Task.propTypes = {
-    /** Composition of the task */
-    task: PropTypes.shape({
-      /** Id of the task */
-      id: PropTypes.string.isRequired,
-      /** Title of the task */
-      title: PropTypes.string.isRequired,
-      /** Current state of the task */
-      state: PropTypes.string.isRequired,
-    }),
-    /** Event to change the task to archived */
-    onArchiveTask: PropTypes.func,
-    /** Event to change the task to pinned */
-    onPinTask: PropTypes.func,
-  };
+  /** Composition of the task */
+  task: PropTypes.shape({
+    /** Id of the task */
+    id: PropTypes.string.isRequired,
+    /** Title of the task */
+    title: PropTypes.string.isRequired,
+    /** Current state of the task */
+    state: PropTypes.string.isRequired,
+  }),
+  /** Event to change the task to archived */
+  onArchiveTask: PropTypes.func,
+  /** Event to change the task to pinned */
+  onPinTask: PropTypes.func,
+};
